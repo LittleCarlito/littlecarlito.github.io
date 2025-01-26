42b836af9a940f31b084b7fc2213b6772c7285c7
Steven Meier <littlecarlitogithub@gmail.com>
2025-01-26 14:57:06 -0600

    Text boxes in out

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 import * as THREE from 'three';
 import { Easing, Tween, update as updateTween } from 'tween';
 
+// Constants
+const PAN_SPEED = 600;
+const ROTATE_SPEED = 300;
+const CONATINER = "container_";
+const LABEL = "label_";
+const TEXT = "text_";
+const NORTH = "north";
+const SOUTH = "south";
+const EAST = "east";
+const WEST = "west";
+const DIRECTIONS =[
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+]
+
 // Values
 const icon_paths = [
     "contact_raised.svg",
@@ -15,7 +32,14 @@ const icon_labels = [
     "work",
     "education",
     "about"
-]
+];
+const icon_text_boxes = [
+    0xe5ce38,
+    0x834eb4,
+    0xb44444,
+    0x25973a,
+    0x3851e5
+];
 // Mouse detection
 const raycaster = new THREE.Raycaster();
 const mouse_location = new THREE.Vector2();
@@ -35,9 +59,11 @@ const camera = new THREE.PerspectiveCamera(
 const texture_loader = new THREE.TextureLoader();
 const renderer = new THREE.WebGLRenderer();
 // Function variables
+const focus_rotation = .7;
 let is_column_left = true;
 let resize_move = false;
 let current_intersected = null;
+let in_tween_map = new Map();
 
 // Setup
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -45,12 +71,12 @@ renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 const container_column = new THREE.Object3D();
-container_column.name = `container_column`
+container_column.name = `${CONATINER}column`
 scene.add(container_column);
 
 for (let i = 0; i < icon_paths.length; i++) {
     const button_container = new THREE.Object3D();
-    button_container.name = `container_${icon_labels[i]}`
+    button_container.name = `${CONATINER}${icon_labels[i]}`
     container_column.add(button_container);
     const button_texture = texture_loader.load(icon_paths[i]);
     button_texture.colorSpace = THREE.SRGBColorSpace;
@@ -60,7 +86,7 @@ for (let i = 0; i < icon_paths.length; i++) {
             map: button_texture,
             transparent: true
         }));
-    button_option.name = `label_${icon_labels[i]}`
+    button_option.name = `${LABEL}${icon_labels[i]}`
     button_option.position.y = i * 3;
     button_container.add(button_option);
 }
@@ -78,10 +104,45 @@ const da_sun = new THREE.DirectionalLight(0xffffff, 10);
 da_sun.position.set(0, 3, -2);
 scene.add(da_sun);
 
+// Text displays
+const text_box_container = new THREE.Object3D();
+scene.add(text_box_container);
+for (let c = 0; c < icon_text_boxes.length; c++) {
+    const box_geometry = new THREE.BoxGeometry(1, 1, .01);
+    const box_material = new THREE.MeshBasicMaterial({ color: icon_text_boxes[c] });
+    const text_box = new THREE.Mesh(box_geometry, box_material);
+    text_box.name = `${TEXT}${icon_labels[c]}`
+    text_box.position.x = -window.innerWidth;
+    text_box.position.y = -(.05 * screen_size.y);
+    text_box_container.add(text_box);
+}
+
+
 // Functions
 
-/*** Swaps the container column sides */
+/** TODO Gets final location of assicated direction given current camera 
+ ** incoming_direction must be a member of DIRECTIONS
+*/
+function get_associated_position(incoming_direction) {
+    if(DIRECTIONS.includes(incoming_direction)){
+        const screen_size = new THREE.Vector2();
+        camera.getViewSize(15, screen_size);
+        switch(incoming_direction) {
+            case NORTH:
+                return screen_size.y;
+            case SOUTH:
+                return -screen_size.y;
+            case EAST:
+                return screen_size.x;
+            case WEST:
+                return -screen_size.x;
+        }
+    } else {
+        // TODO Log that given direction is not supported
+    }
+}
 
+/*** Swaps the container column sides */
 function swap_column_sides() {
     const determined_size = new THREE.Vector2();
     camera.getViewSize(15, determined_size);
@@ -92,14 +153,99 @@ function swap_column_sides() {
 
     // Move column across the screen
     new Tween(container_column.position)
-    .to({ x: x_position, y: y_position}, 600)
+    .to({ x: x_position, y: y_position}, PAN_SPEED)
     .easing(Easing.Elastic.Out)
     .start();
     // Rotate the column as it moves
     new Tween(container_column.rotation)
-    .to({ y: y_rotation}, 300)
+    .to({ y: y_rotation}, ROTATE_SPEED)
     .easing(Easing.Exponential.Out)
     .start();
+}
+
+// TODO Change swap_column calling logic
+//          Should require selection to move left
+//          No selection click when right
+//              Moves column back left
+//              Moves active text box left off screen
+//          Selection while right
+//              Keeps column on the right
+//              Moves active text box down off screen
+//              Moves selected text box to active spot
+
+let focused_text_name = "";
+/** Brings the text box associated with the given name into focus
+ ** container column MUST be on the right side
+ */
+function focus_text_box(incoming_name) {
+    if(!is_column_left) {
+        // Get screen size
+        const new_size = new THREE.Vector2();
+        camera.getViewSize(15, new_size);
+        // Get text box name
+        const found_index = incoming_name.indexOf('_');
+        const sub_name = incoming_name.substring(found_index + 1);
+        // If existing focus text box move it
+        if(focused_text_name != "") {
+            lose_focus_text_box(SOUTH);
+        }
+        focused_text_name = TEXT + sub_name;
+        // Get and move text box
+        const selected_text_box = text_box_container.getObjectByName(focused_text_name);
+        new Tween(selected_text_box.position)
+        .to({ x: -(new_size.x * .2) }, 285)
+        .easing(Easing.Sinusoidal.Out)
+        .start()
+    } else {
+        lose_focus_text_box(WEST);
+        // TODO Log that container_column MUST be right side for focus text box
+    }
+}
+
+//  Method to tween focused_text_name to offscreen and set to empty string
+function lose_focus_text_box(move_direction = "") {
+    if(focused_text_name != "") {
+        if(move_direction == "" || DIRECTIONS.includes(move_direction)) {
+            const new_size = new THREE.Vector2();
+            camera.getViewSize(15, new_size);
+            const existing_focus_box = text_box_container.getObjectByName(focused_text_name);
+            if(move_direction == "") {
+                existing_focus_box.position.x = -(new_size.x);
+            } else {
+                // Tween in given direction off screen
+                const previous_position = existing_focus_box.position;
+                const move_position = get_associated_position(move_direction);
+                switch(move_direction) {
+                    case NORTH, SOUTH:
+                        new Tween(existing_focus_box.position)
+                        .to({ y: move_position }, PAN_SPEED)
+                        .easing(Easing.Sinusoidal.Out)
+                        .start()
+                        .onComplete(() => {
+                            existing_focus_box.position.x = -new_size.x;
+                            existing_focus_box.position.y = previous_position.y;
+                        });
+                        break;
+                    case EAST, WEST:
+                        new Tween(existing_focus_box.position)
+                        .to({ x: move_position }, PAN_SPEED)
+                        .easing(Easing.Sinusoidal.Out)
+                        .start()
+                        .onComplete(() => {
+                            existing_focus_box.position.x = -new_size.x;
+                        });
+                        break;
+                }
+            }
+            // Lose focus on box
+            focused_text_name = "";
+        } else {
+            // TODO Log passed in move direction isn't supported
+        }
+    } else {
+        // TODO Log that there is no focused text box to lose focus on
+    }
+
 }
 
 function animate() {
@@ -122,6 +268,9 @@ function animate() {
         .easing(Easing.Exponential.Out)
         .start();
 
+        // TODO Move text boxes out of window
+        text_box.position.x = - found_size.x;
+
         resize_move = false;
     }
     renderer.render(scene, camera);
@@ -134,8 +283,6 @@ function get_intersect_list(e) {
     return raycaster.intersectObject(container_column, true);
 }
 
-// TODO Get this to be label specific so tweens can start on other labels at once
-let in_tween_map = new Map();
 function handle_hover(e) {
     const found_intersections = get_intersect_list(e);
     if(found_intersections.length > 0) {
@@ -144,7 +291,6 @@ function handle_hover(e) {
         if(current_intersected !== intersected_object) {
             // Reset previously inersected object if one existed
             if(current_intersected){
-                // TODO Switch this to be a tween
                 let deselected_rotation = 0;
                 new Tween(current_intersected.rotation)
                 .to({ y: deselected_rotation})
@@ -155,13 +301,12 @@ function handle_hover(e) {
             current_intersected = intersected_object;
         }
         // Apply rotation to current
-        // TODO Switch this to be a tween
-        let final_rotation = is_column_left ? -1 : 1;
+        let final_rotation = is_column_left ? -(focus_rotation) : (focus_rotation);
         // Determine if there is an existing in tween for this object
         let in_tween = in_tween_map.get(object_name);
         if(in_tween == null) {
             in_tween = new Tween(current_intersected.rotation)
-            .to({ y: final_rotation}, 600)
+            .to({ y: final_rotation}, 400)
             .easing(Easing.Sinusoidal.In)
             .start()
             .onComplete(() => in_tween_map.delete(object_name));
@@ -174,7 +319,6 @@ function handle_hover(e) {
 
 function reset_previous_intersected() {
     if(current_intersected) {
-        // TODO Switch this to be a tween
         let deselected_rotation = 0;
         new Tween(current_intersected.rotation)
         .to({ y: deselected_rotation})
@@ -201,11 +345,14 @@ window.addEventListener('mousedown', (e) => {
 // TODO Handle mouse up
 window.addEventListener('mouseup', (e) => {
     const found_intersections = get_intersect_list(e, "clicked up");
-    found_intersections.forEach(i => (console.log(`${i.object.name} clicked up`)));
-    // TODO Temporary (though will want to include doing this when info pops up)
-    reset_previous_intersected();
-    swap_column_sides();
+    if(found_intersections.length > 0){
+        const intersected_object = found_intersections[0].object;
+        (console.log(`${intersected_object.name} clicked up`))
+        reset_previous_intersected();
+        // TODO Provide label nameto focus_text_box
+        swap_column_sides();
+        focus_text_box(intersected_object.name);
+    }
 });
 
-// TODO Handle mouse movement
 window.addEventListener('mousemove', handle_hover)
