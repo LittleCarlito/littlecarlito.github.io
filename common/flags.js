fa1fd43f9541b7e42ff68eab76784c57b80c055c
LittleCarlito <LittleCarlitoGithub@gmail.com>
2025-02-26 16:20:02 -0600

    Spotlight debug to own flag

--- common/flags.js
+++ common/flags.js
@@ -2,7 +2,7 @@ export const FLAGS = {
     // Functionality flags
     CONSTRUCTION_GREETING: true,
     LAYER: true,
-    VISUAL_DEBUG: false,
+    SPOTLIGHT_VISUAL_DEBUG: false,
     LABEL_VISUAL_DEBUG: false,
     // Log flags
     SELECT_LOGS: false,
