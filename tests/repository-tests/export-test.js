32a930eca6dcc2f1f5887835f25620aaf84a160f
LittleCarlito <LittleCarlitoGithub@gmail.com>
2025-03-20 23:56:41 -0500

    fix: last last try

--- tests/repository-tests/export-test.js
+++ tests/repository-tests/export-test.js
@@ -1,5 +1,5 @@
 // Test importing THREE and RAPIER from the blorkpack package
-const { THREE, RAPIER } = require('../../packages/blorkpack/dist/index.js');
+import { THREE, RAPIER } from '../../packages/blorkpack/dist/index.js';
 
 // Basic validation tests that don't directly access THREE objects
 test('THREE object is available', () => {
