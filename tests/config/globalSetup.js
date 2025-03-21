a7cd7407af471d307c6fc93fdbde5768ca6512ec
LittleCarlito <LittleCarlitoGithub@gmail.com>
2025-03-20 23:31:17 -0500

    fix: lint directory creation

--- tests/config/globalSetup.js
+++ tests/config/globalSetup.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async () => {
-  // Create lint-tests directory if it doesn't exist
-  const lintTestsDir = path.join(process.cwd(), 'tests/lint-tests');
-  if (!fs.existsSync(lintTestsDir)) {
-    console.log(`Creating directory: ${lintTestsDir}`);
-    fs.mkdirSync(lintTestsDir, { recursive: true });
-  }
+	// Create lint-tests directory if it doesn't exist
+	const lintTestsDir = path.join(process.cwd(), 'tests/lint-tests');
+	if (!fs.existsSync(lintTestsDir)) {
+		console.log(`Creating directory: ${lintTestsDir}`);
+		fs.mkdirSync(lintTestsDir, { recursive: true });
+	}
 }; 
