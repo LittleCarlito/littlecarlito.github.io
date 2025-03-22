291801f5bad26463eda142188d04890882b3de2a
LittleCarlito <LittleCarlitoGithub@gmail.com>
2025-03-22 13:43:50 -0500

    fix: merging main

---
+++ fix_workspace.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+// Process all dependency types
+const depTypes = ["dependencies", "devDependencies", "peerDependencies", "optionalDependencies"];
+
+depTypes.forEach(type => {
+	if (pkg[type]) {
+		Object.keys(pkg[type]).forEach(dep => {
+			// Replace workspace: with * for all versions
+			if (pkg[type][dep].startsWith("workspace:")) {
+				pkg[type][dep] = "*";
+			}
+		});
+	}
+});
+
+// Extra check to handle nested workspace references
+const pkgStr = JSON.stringify(pkg, null, 2);
+if (pkgStr.includes("workspace:")) {
+	console.log("Found additional workspace references, fixing with regex");
+	const fixedPkgStr = pkgStr.replace(/"workspace:[^"]*"/g, '"*"');
+	fs.writeFileSync("package.json", fixedPkgStr);
+} else {
+	fs.writeFileSync("package.json", pkgStr);
+}
+
+console.log("Successfully removed all workspace: references"); 
