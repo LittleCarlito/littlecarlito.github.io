c6739308863f0969b60f63b567ea8c18f5d86937
LittleCarlito <LittleCarlitoGithub@gmail.com>
2025-06-16 08:32:12 -0500

    refactor(blorktools): rig_debugger removed

--- packages/blorktools/src/index.js
+++ packages/blorktools/src/index.js
@@ -2,13 +2,7 @@
  * Blorktools - 3D Asset Development Toolset
  * Main entry point for the package
  */
-// Re-export core functionality from each tool
-export * from './rig_debugger/index.js';
 
 // Export individual tools
 export const tools = {
-	// Rig Debugger Tool
-	rigDebugger: {
-		init: () => import('./rig_debugger/index.js').then(module => module.default)
-	}
 };
